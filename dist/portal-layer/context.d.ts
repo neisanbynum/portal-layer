85ca762669194265296cdc6d99c623d62cfd6365
Nathan B <emmanuel.n.bynum2024@outlook.com>
2025-04-27 21:40:14 -0500

    Included Portal for components using Portal Layer

--- dist/portal-layer/context.d.ts
+++ dist/portal-layer/context.d.ts
@@ -1,4 +1,5 @@
 import React from "react";
+import { PortalContextValues } from "./types";
 export declare const PortalLayerContext: React.Context<HTMLDivElement | null>;
-declare const usePortalLayer: () => HTMLDivElement;
+declare const usePortalLayer: () => PortalContextValues;
 export default usePortalLayer;
--- dist/portal-layer/types.d.ts
+++ dist/portal-layer/types.d.ts
@@ -3,4 +3,5 @@ export type PortalLayerProperties = {
     children?: React.ReactNode;
     id?: string;
 };
+export type PortalContextValues = HTMLDivElement;
 export type PortalProperties = React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
